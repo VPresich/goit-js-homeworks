d5463a441c01131e9449909a48800abed59d7f92
VPresich <valya.alexandrova.presich@gmail.com>
2023-10-07 05:36:20 +0200

    Добавлены тесты

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,6 @@
 "use strict";
 import isNumber from "./module-01/auxiliary.js";
 export default function getShippingMessage(country, price, deliveryFee) {
-  const price = parseFloat(price);
-  const deliveryFee = parseFloat(deliveryFee);
   if (!isNumber(price)) return "Error in price";
   if (!isNumber(deliveryFee)) return "Error in deliveryFee";
   return `Shipping to ${country} will cost ${price + deliveryFee} credits`;
--- js/task-3.js
+++ js/task-3.js
@@ -12,6 +12,7 @@ export default function getElementWidth(content, padding, border) {
   return contentWidth + 2 * paddingWidth + 2 * borderWidth;
 }
 
+//for test
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
