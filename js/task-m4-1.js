b927cf62c83ae9f808b42756cdd6dd3dd9e74418
VPresich <valya.alexandrova.presich@gmail.com>
2023-10-08 14:18:05 +0200

    Добавлена home-4

--- js/task-m4-1.js
+++ js/task-m4-1.js
@@ -13,26 +13,18 @@
 // товарів в об’єкті products і повернути true, якщо вона менше або дорівнює containerSize, і false, якщо ні.
 
 export default function isEnoughCapacity(products, containerSize) {
-  const values = Objects.values(products);
-  const productsSize = 0;
+  const values = Object.values(products);
+  let productsSize = 0;
   for (const value of values) {
     productsSize += value;
   }
   return containerSize >= productsSize;
 }
 
-console.log(
-  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-); // true
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
 
-console.log(
-  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
-); // false
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
 
-console.log(
-  isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
-); // true
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)); // true
 
-console.log(
-  isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
-); // false
+console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)); // false
